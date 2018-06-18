492b54b235675e5fa75ba4fc83d14f83dfd0ea8a
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-06-18 17:51:48 -0500

    Commit 1 - selecting datatypes.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("The javascript is loaded.");
+
+// Q1. A light switch that can be either on or off.
+// -Boolean
+// Q2. A user's email address.
+// -String
+// Q3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// -Array
+// Q4. A list of student names from our class.
+// -Object
+// Q5. A list of student names from our class, each with a location.
+// -Object -Object
+// Q6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// -Object -Object -Array
